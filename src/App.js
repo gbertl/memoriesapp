2258027af46417648eea68f49bcc459a34e8c2fa
gilbertlc <gilbert.lacasandile@gmail.com>
2021-08-11 16:04:32 +0800

    final build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-function App() {
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+import memories from "./images/memories.png";
+import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { getPosts } from "./actions/posts";
+
+const App = () => {
+  const [currentId, setCurrentId] = useState(null);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+            className={classes.mainContainer}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/posts.js
@@ -0,0 +1,27 @@
+import {
+  FETCH_ALL,
+  LIKE,
+  CREATE,
+  UPDATE,
+  DELETE,
+} from "../constants/actionTypes";
+
+const reducer = (posts = [], action) => {
+  switch (action.type) {
+    case DELETE:
+      return posts.filter((p) => p._id !== action.payload);
+    case UPDATE:
+    case LIKE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case FETCH_ALL:
+      return action.payload;
+    case CREATE:
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
+
+export default reducer;
